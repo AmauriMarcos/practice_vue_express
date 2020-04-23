ef45f74d7def79a1f5add543ffeee61462b57e55
AmauriMarcos <amaurisantos.m@gmail.com>
2020-04-23 12:10:11 +0200

    setup express router

--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ bodyParser.urlencoded({extended: true});
 app.use(bodyParser.json());
 app.use(cors());
 
+
+const posts = require('./routes/api/posts');
+app.use('/api/posts', posts);
+
+
 const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
